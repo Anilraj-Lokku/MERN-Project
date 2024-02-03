d04f988396ba08920464343bd199b8e7daf7077b
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-02-03 20:19:29 +0530

    MERN PROJECT FIRST COMMIT

---
+++ client/src/BookingWidget.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useContext } from "react"; // Import useContext
+import { differenceInCalendarDays } from "date-fns";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "./UserContext.jsx";
+
+export default function BookingWidgets({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const [name, setName] = useState(""); // Added state for name
+  const [phone, setPhone] = useState(""); // Added state for phone
+  const [redirect, setRedirect] = useState("");
+
+  const { user } = useContext(UserContext); // Access user context
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+    }
+  }, [user]);
+
+  let numberOfDays = 0;
+  if (checkIn && checkOut) {
+    numberOfDays = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
+    );
+  }
+
+  async function bookThisPlace() {
+    const response = await axios.post("/bookings", {
+      checkIn,
+      checkOut,
+      numberOfGuests,
+      name,
+      phone,
+      place: place._id,
+      price: numberOfDays * place.price,
+    });
+    const bookingId = response.data._id;
+    setRedirect(`/account/bookings/${bookingId}`);
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  return (
+    <div className="bg-white shadow p-4 rounded-2xl">
+      <div className="text-2xl text-center">
+        Price: ₹{place.price} / per Day
+      </div>
+      <div className="border rounded-2xl mt-4">
+        <div className="flex">
+          <div className="py-3 px-4">
+            <label>Check in:</label>
+            <input
+              type="date"
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+            />
+          </div>
+          <div className="py-3 px-4 border-l">
+            <label>Check out:</label>
+            <input
+              type="date"
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+            />
+          </div>
+        </div>
+        <div className="py-3 px-4 border-t">
+          <label>Number of guests:</label>
+          <input
+            type="number"
+            value={numberOfGuests}
+            onChange={(ev) => setNumberOfGuests(ev.target.value)}
+          />
+        </div>
+        {numberOfDays > 0 && (
+          <div className="py-3 px-4 border-t">
+            <label>Your full name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+            />
+            <label>Phone number:</label>
+            <input
+              type="tel"
+              value={phone}
+              onChange={(ev) => setPhone(ev.target.value)}
+            />
+          </div>
+        )}
+      </div>
+      <button onClick={bookThisPlace} className="primary mt-4">
+        Book this place
+        {numberOfDays > 0 && <span> ${numberOfDays * place.price}</span>}
+      </button>
+    </div>
+  );
+}
---
+++ client/src/CustomImage.jsx
@@ -0,0 +1,7 @@
+export default function CustomImage({ src, ...rest }) {
+  src =
+    src && src.includes("https://")
+      ? src
+      : "http://localhost:3000/uploads/" + src;
+  return <img {...rest} src={src} alt={""} />;
+}
