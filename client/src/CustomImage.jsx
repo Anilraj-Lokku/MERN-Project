43f317c46fd5dbccaab530d16d547694154541e4
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-02-04 05:45:28 +0530

    10th Commit

--- client/src/CustomImage.jsx
+++ client/src/CustomImage.jsx
@@ -1,6 +1,6 @@
 export default function CustomImage({ src, ...rest }) {
   src =
-    src && src.includes("https://")
+    src && src.includes("http://")
       ? src
       : "http://localhost:3000/uploads/" + src;
   return <img {...rest} src={src} alt={""} />;
