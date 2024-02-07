4e9003c76ea0f1b3c0ec3faf7dca938005067036
Anilraj Lokku <128417620+Anilraj-Lokku@users.noreply.github.com>
2024-02-08 00:53:07 +0530

    Update AccountPage.jsx

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -6,44 +6,59 @@ import PlacesPage from "./PlacesPage";
 import AccountNav from "../AccountNav";
 
 export default function AccountPage() {
-  const [redirect, setRedirect] = useState(null);
-  const { ready, user, setUser } = useContext(UserContext);
+  // State variables
+  const [redirect, setRedirect] = useState(null); // State variable for redirecting
+  const { ready, user, setUser } = useContext(UserContext); // Context for user data
 
-  // We Can Also Use This Instead Of || (subpage === undefined && type === 'profile')
+  // Get subpage parameter from URL
   let { subpage } = useParams();
   if (subpage === undefined) {
-    subpage = "profile";
+    subpage = "profile"; // Default to "profile" if subpage parameter is not provided
   }
 
+  // Logout function
   async function logout() {
+    // Send logout request to server
     await axios.post("/logout");
+    // Redirect to home page after logout
     setRedirect("/");
+    // Clear user data from context
     setUser(null);
   }
 
+  // Loading state
   if (!ready) {
-    return "Loading...";
+    return "Loading..."; // Show loading message if user data is not yet available
   }
 
+  // Redirect to login page if user is not logged in
   if (ready && !user && !redirect) {
-    return <Navigate to={"/login"} />;
+    return <Navigate to={"/login"} />; // Navigate to login page
   }
 
+  // Redirect to specified page if needed
   if (redirect) {
-    return <Navigate to={redirect} />;
+    return <Navigate to={redirect} />; // Navigate to the specified URL
   }
 
   return (
     <div>
+      {/* Account navigation menu */}
       <AccountNav />
+
+      {/* Profile page */}
       {subpage === "profile" && (
         <div className="text-center max-w-lg mx-auto ">
+          {/* Display user information */}
           Logged in as {user.name}({user.email})<br />
+          {/* Logout button */}
           <button onClick={logout} className="primary max-w-sm mt-3 mb-5">
             Logout
           </button>
         </div>
       )}
+
+      {/* Places page */}
       {subpage === "places" && <PlacesPage />}
     </div>
   );
